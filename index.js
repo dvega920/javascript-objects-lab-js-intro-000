2369a4649515f047232908c9e6d99e5edea8416f
david.vega@snhu.edu <david.vega@snhu.edu>
2018-09-03 13:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {key: 'value'};
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
-  return object
+  return object;
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
