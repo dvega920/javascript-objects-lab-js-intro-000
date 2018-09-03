5dbb50699dca1473c09bdd41db2e07a8c9a49ab0
david.vega@snhu.edu <david.vega@snhu.edu>
2018-09-03 13:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
   return object
 }
 
-// function deleteFromObjectByKey(object, key){
-//   delete object.key;
-//   return object
-// }
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete object[key];
+  return newObj
+}
